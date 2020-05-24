8d5120619ea20d5146e54ea61c9a72f5c81ea7cb
Arjentix <arjentix@gmail.com>
2020-05-24 16:47:42 +0300

    Added priority

--- src/main/java/ru/arjentix/gotl/lexer/LexemType.java
+++ src/main/java/ru/arjentix/gotl/lexer/LexemType.java
@@ -4,36 +4,43 @@ import java.util.regex.Pattern;
 
 public enum LexemType {
 
-  VALAR_MORGHULIS("Valar Morghulis"),
-  VALAR_DOHAERIS("Valar Dohaeris"),
-  DAENERIS("Daeneris\\:"),
-  TYRION("Tyrion\\:"),
-  CERCEI("Cercei\\:"),
-  ARYA("Arya\\:"),
-  JON("Jon\\:"),
-  YGRITTE("Ygritte\\:"),
-  ASSIGN_OP("="),
-  INPUT_OUTPUT_OP("--"),
-  DIGIT("0|([1-9][0-9]*)"),
-  OP("\\+|\\-|\\*|\\/"),
-  LOGIC_OP(">|<|==|>=|<="),
-  CONST_STRING("\".*\""),
-  IF_KW("if"),
-  WHILE_KW("while"),
-  SEMICOLON("\\;"),
-  OPEN_PARANTH("\\("),
-  CLOSE_PARANTH("\\)"),
-  OPEN_BRACKET("\\{"),
-  CLOSE_BRACKET("\\}"),
-  VAR("[a-zA-z]+");
+  VALAR_MORGHULIS("Valar Morghulis", -1),
+  VALAR_DOHAERIS("Valar Dohaeris", -1),
+  DAENERIS("Daeneris\\:", -1),
+  TYRION("Tyrion\\:", -1),
+  CERCEI("Cercei\\:", -1),
+  ARYA("Arya\\:", -1),
+  JON("Jon\\:", -1),
+  YGRITTE("Ygritte\\:", -1),
+  ASSIGN_OP("=", 2),
+  INPUT_OUTPUT_OP("--", 2),
+  DIGIT("0|([1-9][0-9]*)", 0),
+  PLUS_MINUS("\\+|\\-", 4),
+  MULT_DIV("\\*|\\/", 3),
+  LOGIC_OP(">|<|==|>=|<=", 2),
+  CONST_STRING("\".*\"", 0),
+  IF_KW("if", 5),
+  WHILE_KW("while", 5),
+  SEMICOLON("\\;", -1),
+  OPEN_PARANTH("\\(", 1),
+  CLOSE_PARANTH("\\)", 1),
+  OPEN_BRACKET("\\{", 1),
+  CLOSE_BRACKET("\\}", 1),
+  VAR("[a-zA-z]+", 0);
 
   private Pattern pattern;
+  private int priority;
 
-  LexemType(String regexp) {
+  LexemType(String regexp, int priority) {
     this.pattern = Pattern.compile(regexp);
+    this.priority = priority;
   }
 
   public Pattern getPattern() {
     return this.pattern;
+  }
+
+  public int getPriority() {
+      return this.priority;
   }
 }
