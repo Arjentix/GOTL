8eefddbe6f9325866888ffe96d1993b53c082ebb
Arjentix <arjentix@gmail.com>
2020-05-28 21:57:02 +0300

    Indent now is 2 spaces instead of 4

--- src/main/java/ru/arjentix/gotl/rpn_translator/RpnTranslator.java
+++ src/main/java/ru/arjentix/gotl/rpn_translator/RpnTranslator.java
@@ -9,128 +9,128 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class RpnTranslator {
-    private List<Token> tokens;
-    private VarTable varTable;
+  private List<Token> tokens;
+  private VarTable varTable;
 
-    private void fromStackToList(Stack<Token> stack, List<Token> list) {
-        while (!stack.empty()) {
-            list.add(stack.pop());
-        }
+  private void fromStackToList(Stack<Token> stack, List<Token> list) {
+    while (!stack.empty()) {
+      list.add(stack.pop());
     }
+  }
 
-    public RpnTranslator(List<Token> tokens, VarTable varTable) {
-        this.tokens = tokens;
-        this.varTable = varTable;
-    }
+  public RpnTranslator(List<Token> tokens, VarTable varTable) {
+    this.tokens = tokens;
+    this.varTable = varTable;
+  }
 
-    public List<Token> getRpn() {
-        List<Token> rpnList = new ArrayList<>();
-        Stack<Token> stack = new Stack<>();
-        Stack<LexemType> exprWithTransitions = new Stack<>();
-        Stack<Integer> whileKwPositions = new Stack<>();
-        int transitionNumber = 0;
+  public List<Token> getRpn() {
+    List<Token> rpnList = new ArrayList<>();
+    Stack<Token> stack = new Stack<>();
+    Stack<LexemType> exprWithTransitions = new Stack<>();
+    Stack<Integer> whileKwPositions = new Stack<>();
+    int transitionNumber = 0;
 
-        for (Token curToken : tokens) {
-            LexemType curType = curToken.getType();
+    for (Token curToken : tokens) {
+      LexemType curType = curToken.getType();
 
-            // Skipping tokens with priority < 0
-            if (curType.getPriority() < 0) {
-                continue;
-            }
+      // Skipping tokens with priority < 0
+      if (curType.getPriority() < 0) {
+        continue;
+      }
 
-            // Processing variables, digits and strings
-            if (curType == LexemType.VAR || curType == LexemType.DIGIT || 
-                curType == LexemType.CONST_STRING) {
-                rpnList.add(curToken);
-                continue;
-            }
+      // Processing variables, digits and strings
+      if (curType == LexemType.VAR || curType == LexemType.DIGIT || 
+          curType == LexemType.CONST_STRING) {
+        rpnList.add(curToken);
+        continue;
+      }
 
-            // Processing open paranth
-            if (curType == LexemType.OPEN_PARANTH) {
-                stack.push(curToken);
-                continue;
-            }
+      // Processing open paranth
+      if (curType == LexemType.OPEN_PARANTH) {
+        stack.push(curToken);
+        continue;
+      }
 
-            // Processing semicolon
-            if (curType == LexemType.SEMICOLON) {
-                fromStackToList(stack, rpnList);
-                continue;
-            }
-
-            // Processing close paranth
-            if (curType == LexemType.CLOSE_PARANTH) {
-                Token top = stack.pop();
-                while (top.getType() != LexemType.OPEN_PARANTH) {
-                    rpnList.add(top);
-                    top = stack.pop();
-                }
-
-                // Inserting false transition
-                if (!exprWithTransitions.empty()) {
-                    rpnList.add(
-                        new Token(LexemType.VAR,
-                                  "_p" + Integer.toString(++transitionNumber)
-                        )
-                    );
-                    rpnList.add(new Token(LexemType.FALSE_TRANSITION, "!F"));
-                }
-
-                continue;
-            }
-
-            // Processing close bracket
-            if (curType == LexemType.CLOSE_BRACKET) {
-                // Setting transition variable
-                if (!exprWithTransitions.empty()) {
-                    varTable.add("_p" + Integer.toString(transitionNumber),
-                                 Integer.toString(rpnList.size())
-                    );
-                    if (exprWithTransitions.lastElement() == LexemType.WHILE_KW) {
-                        String transVar = "_p" +
-                                          Integer.toString(++transitionNumber);
-                        rpnList.add(new Token(LexemType.VAR, transVar));
-                        rpnList.add(new Token(LexemType.UNCONDITIONAL_TRANSITION, "!"));
-                        varTable.add(transVar,
-                                    Integer.toString(whileKwPositions.pop())
-                        );
-                    }
-                }
-
-                continue;
-            }
-
-            // Processing if and while
-            if (curType == LexemType.IF_KW || curType == LexemType.WHILE_KW) {
-                exprWithTransitions.push(curType);
-                if (curType == LexemType.WHILE_KW) {
-                    whileKwPositions.push(rpnList.size());
-                }
-                continue;
-            }
-
-            // Processing other types
-            if (stack.empty() ||
-                stack.peek().getType().getPriority() < curType.getPriority()) {
-                stack.push(curToken);
-            }
-            else {
-                Token top = stack.pop();
-                while (!stack.empty() &&
-                       top.getType().getPriority() >= curType.getPriority()) {
-                    rpnList.add(top);
-                    top = stack.pop();
-                }
-                if (top != null) { // Adding last element
-                    rpnList.add(top);
-                }
-
-                stack.push(curToken);
-            }
-        }
-
+      // Processing semicolon
+      if (curType == LexemType.SEMICOLON) {
         fromStackToList(stack, rpnList);
+        continue;
+      }
 
-        return rpnList;
+      // Processing close paranth
+      if (curType == LexemType.CLOSE_PARANTH) {
+        Token top = stack.pop();
+        while (top.getType() != LexemType.OPEN_PARANTH) {
+          rpnList.add(top);
+          top = stack.pop();
+        }
+
+        // Inserting false transition
+        if (!exprWithTransitions.empty()) {
+            rpnList.add(
+              new Token(LexemType.VAR,
+                        "_p" + Integer.toString(++transitionNumber)
+              )
+            );
+            rpnList.add(new Token(LexemType.FALSE_TRANSITION, "!F"));
+        }
+
+        continue;
+      }
+
+      // Processing close bracket
+      if (curType == LexemType.CLOSE_BRACKET) {
+        // Setting transition variable
+        if (!exprWithTransitions.empty()) {
+          varTable.add("_p" + Integer.toString(transitionNumber),
+                        Integer.toString(rpnList.size())
+          );
+          if (exprWithTransitions.lastElement() == LexemType.WHILE_KW) {
+            String transVar = "_p" +
+                              Integer.toString(++transitionNumber);
+            rpnList.add(new Token(LexemType.VAR, transVar));
+            rpnList.add(new Token(LexemType.UNCONDITIONAL_TRANSITION, "!"));
+            varTable.add(transVar,
+                         Integer.toString(whileKwPositions.pop())
+            );
+          }
+        }
+
+        continue;
+      }
+
+      // Processing if and while
+      if (curType == LexemType.IF_KW || curType == LexemType.WHILE_KW) {
+        exprWithTransitions.push(curType);
+        if (curType == LexemType.WHILE_KW) {
+            whileKwPositions.push(rpnList.size());
+        }
+        continue;
+      }
+
+      // Processing other types
+      if (stack.empty() ||
+          stack.peek().getType().getPriority() < curType.getPriority()) {
+          stack.push(curToken);
+      }
+      else {
+        Token top = stack.pop();
+        while (!stack.empty() &&
+                top.getType().getPriority() >= curType.getPriority()) {
+          rpnList.add(top);
+          top = stack.pop();
+        }
+        if (top != null) { // Adding last element
+          rpnList.add(top);
+        }
+
+        stack.push(curToken);
+      }
     }
-    
+
+    fromStackToList(stack, rpnList);
+
+    return rpnList;
+  }
+
 }
--- src/main/java/ru/arjentix/gotl/vartable/VarTable.java
+++ src/main/java/ru/arjentix/gotl/vartable/VarTable.java
@@ -5,64 +5,64 @@ import java.util.Map;
 
 public class VarTable {
 
-    private class VarData {
-        public String type;
-        public String value;
+  private class VarData {
+    public String type;
+    public String value;
 
-        public VarData(String type, String value) {
-            this.type = type;
-            this.value = value;
-        }
-
-        public String toString() {
-            return type + ", " + value;
-        }
-    }
-
-    private HashMap<String, VarData> hashMap;
-
-    public VarTable() {
-        hashMap = new HashMap<>();
-    }
-
-    public void add(String var, String value) {
-        hashMap.put(var, new VarData("int", value));
-    }
-
-    public void add(String var, String type, String value) {
-        hashMap.put(var, new VarData(type, value));
-    }
-
-    public String getType(String var) {
-        return hashMap.get(var).type;
-    }
-
-    public String getValue(String var) {
-        return hashMap.get(var).value;
-    }
-
-    public void setType(String var, String type) {
-        hashMap.get(var).type = type;
-    }
-
-    public void setValue(String var, String value) {
-        hashMap.get(var).value = value;
+    public VarData(String type, String value) {
+        this.type = type;
+        this.value = value;
     }
 
     public String toString() {
-        StringBuilder builder = new StringBuilder("{");
-        boolean first = true;
-
-        for (Map.Entry<String, VarData> entry : hashMap.entrySet()) {
-            if (!first) {
-                builder.append(", ");
-            }
-            builder.append("[" + entry.getKey() + " : " + entry.getValue() + "]");
-            first = false;
-        }
-        builder.append("}");
-
-        return builder.toString();
+        return type + ", " + value;
     }
-    
+  }
+
+  private HashMap<String, VarData> hashMap;
+
+  public VarTable() {
+    hashMap = new HashMap<>();
+  }
+
+  public void add(String var, String value) {
+    hashMap.put(var, new VarData("int", value));
+  }
+
+  public void add(String var, String type, String value) {
+    hashMap.put(var, new VarData(type, value));
+  }
+
+  public String getType(String var) {
+    return hashMap.get(var).type;
+  }
+
+  public String getValue(String var) {
+    return hashMap.get(var).value;
+  }
+
+  public void setType(String var, String type) {
+    hashMap.get(var).type = type;
+  }
+
+  public void setValue(String var, String value) {
+    hashMap.get(var).value = value;
+  }
+
+  public String toString() {
+    StringBuilder builder = new StringBuilder("{");
+    boolean first = true;
+
+    for (Map.Entry<String, VarData> entry : hashMap.entrySet()) {
+      if (!first) {
+        builder.append(", ");
+      }
+      builder.append("[" + entry.getKey() + " : " + entry.getValue() + "]");
+      first = false;
+    }
+    builder.append("}");
+
+    return builder.toString();
+  }
+
 }
