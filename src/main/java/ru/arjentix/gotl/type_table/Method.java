0a40ba97013fc3c20b876cfb71c52213e331a626
Arjentix <arjentix@gmail.com>
2020-05-30 18:40:37 +0300

    Added TypeTable

---
+++ src/main/java/ru/arjentix/gotl/type_table/Method.java
@@ -0,0 +1,31 @@
+package ru.arjentix.gotl.type_table;
+
+import java.util.List;
+import java.util.function.BiFunction;
+
+public class Method {
+    private String name;
+    private List<String> paramTypes;
+    private String returnType;
+    private BiFunction<Object, Object, Object> implementation;
+
+    public Method(String name, List<String> paramTypes, String returnType,
+                  BiFunction<Object, Object, Object> implementation) {
+      this.name = name;
+      this.paramTypes = paramTypes;
+      this.returnType = returnType;
+      this.implementation = implementation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getParamTypes() {
+        return paramTypes;
+    }
+
+    public Object invoke(Object arg0, Object arg1) {
+        return implementation.apply(arg0, arg1);
+    }
+}
---
+++ src/main/java/ru/arjentix/gotl/type_table/TypeTable.java
@@ -0,0 +1,12 @@
+package ru.arjentix.gotl.type_table;
+
+import ru.arjentix.gotl.type_table.Method;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class TypeTable extends HashMap<String, List<Method>> {
+  public TypeTable() {
+    super();
+  }
+}
