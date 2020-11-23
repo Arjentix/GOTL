faff136b8b899bc431a1729b45fcd315ff1dd157
Arjentix <arjentix@gmail.com>
2020-11-23 21:15:22 +0300

    Added toString()

--- src/main/java/ru/arjentix/gotl/function_table/Function.java
+++ src/main/java/ru/arjentix/gotl/function_table/Function.java
@@ -6,19 +6,23 @@ import ru.arjentix.gotl.token.Token;
 
 public class Function {
   private int argsCount;
-  private List<Token> rpn;
+  private List<Token> body;
 
-  public Function(int argsCount, List<Token> rpn) {
+  public Function(int argsCount, List<Token> body) {
     this.argsCount = argsCount;
-    this.rpn = rpn;
+    this.body = body;
   }
 
   public int getArgsCount() {
     return argsCount;
   }
 
-  public List<Token> getRpn() {
-    return rpn;
+  public List<Token> getBody() {
+    return body;
+  }
+
+  public String toString() {
+    return "args count = " + argsCount + ", body = " + body;
   }
 
 }
--- src/main/java/ru/arjentix/gotl/function_table/FunctionTable.java
+++ src/main/java/ru/arjentix/gotl/function_table/FunctionTable.java
@@ -1,6 +1,7 @@
 package ru.arjentix.gotl.function_table;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class FunctionTable extends HashMap<String, Function> {
   private static FunctionTable instance;
@@ -15,5 +16,22 @@ public class FunctionTable extends HashMap<String, Function> {
     }
 
     return instance;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder("{");
+    boolean first = true;
+
+    for (Map.Entry<String, Function> entry : super.entrySet()) {
+      if (!first) {
+        builder.append(", ");
+      }
+      builder.append("[" + entry.getKey() + " : " + entry.getValue() + "]");
+      first = false;
+    }
+    builder.append("}");
+
+    return builder.toString();
   }
 }
