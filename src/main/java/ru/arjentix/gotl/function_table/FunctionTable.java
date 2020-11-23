cf1a512641fe396efd5f61f859bcf1b000478d37
Arjentix <arjentix@gmail.com>
2020-11-23 19:14:16 +0300

    FunctionTable is a singleton now

--- src/main/java/ru/arjentix/gotl/function_table/FunctionTable.java
+++ src/main/java/ru/arjentix/gotl/function_table/FunctionTable.java
@@ -3,7 +3,17 @@ package ru.arjentix.gotl.function_table;
 import java.util.HashMap;
 
 public class FunctionTable extends HashMap<String, Function> {
-  public FunctionTable() {
+  private static FunctionTable instance;
+
+  private FunctionTable() {
     super();
+  }
+
+  public static FunctionTable getInstance() {
+    if (instance == null) {
+      instance = new FunctionTable();
+    }
+
+    return instance;
   }
 }
